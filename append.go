0343aeed961bc5281b08640a8e2125ef81ced329
prmoreira23 <prm.gredes@gmail.com>
2018-10-17 15:12:10 -0400

    append

---
+++ append.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main(){
+    var s []int
+    printSlices(s)
+
+    s = append(s, 0)
+    printSlices(s)
+
+    s = append(s, 1)
+    printSlices(s)
+
+    s = append(s, 2, 3, 4)
+    printSlices(s)
+
+
+}
+
+func printSlices(s []int){
+    fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
